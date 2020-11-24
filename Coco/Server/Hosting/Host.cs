190a9d5d8d577be5a51f9cdd7fca2936455975db
CC Liu <Y-Y-Liu@hotmail.com>
2020-11-24 20:31:33 +0800

    修改范式

---
+++ Coco/Server/Hosting/Host.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coco.Server.Hosting
+{
+    public static class Host
+    {
+
+        public static ICocoHostBuilder CreateDefaultBuilder()
+        {
+            throw null;
+        }
+
+        public static ICocoHostBuilder CreateDefaultBuilder(string[] args)
+        {
+            throw null;
+        }
+    }
+}
---
+++ Coco/Server/Hosting/ICocoHostBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coco.Server.Hosting
+{
+    public class ICocoHostBuilder
+    {
+
+    }
+}
