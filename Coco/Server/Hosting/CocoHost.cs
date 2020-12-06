3fa7350543e62e70c8ca0e959fb623a021a93161
CC Liu <Y-Y-Liu@hotmail.com>
2020-12-06 22:43:34 +0800

    升级到.net 5

--- Coco/Server/Hosting/CocoHost.cs
+++ Coco/Server/Hosting/CocoHost.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Coco.Server.Hosting
