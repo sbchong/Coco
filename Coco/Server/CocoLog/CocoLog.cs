9ce376b25f66f090f855e45f866df92980126d68
CC Liu <Y-Y-Liu@hotmail.com>
2020-12-18 23:50:50 +0800

    修复日志没提交的bug

---
+++ Coco/Server/CocoLog/CocoLog.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Coco.Server.CocoLog
+{
+    public static class CocoLog
+    {
+        private static readonly string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".log");
+
+        private static readonly string logFileName = Path.Combine(logFilePath, "running.log");
+
+        private static bool init = false;
+
+        private static bool ok = true;
+
+        private static List<string> logs = new List<string>();
+
+        public static void Init()
+        {
+            if (!Directory.Exists(logFilePath))
+            {
+                Directory.CreateDirectory(logFilePath);
+                File.Create(logFileName);
+                init = true;
+            }
+            else
+            {
+                if (File.Exists(logFileName))
+                {
+                    File.Create(logFileName);
+                    init = true;
+                }
+                else
+                {
+                    init = true;
+                }
+            }
+        }
+
+        public static void LogInformation(string infomation)
+        {
+            Console.WriteLine(infomation);
+            if (!init)
+            {
+                Init();
+            }
+            logs.Add($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}] {infomation}\n");
+            if (ok)
+            {
+                new Thread(WriteToFile).Start();
+            }
+        }
+
+        public static void WriteToFile()
+        {
+            using var fs = new FileStream(logFileName, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+            foreach (var log in logs)
+            {
+                fs.Write(Encoding.UTF8.GetBytes(log));
+            }
+            ok = true;
+        }
+    }
+}
